9ac259fdf9c4858be8bfe1344fcbfe9b9065b653
Vincent Hokie <vincenthokie@gmail.com>
2017-08-16 15:55:49 +0300

    the soon-to-be-outdated 'Form' class now the newere 'FlaskForm'

--- app/forms.py
+++ app/forms.py
@@ -1,28 +1,28 @@
 __author__ = 'MacUser'
 
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, BooleanField, TextAreaField, DateField
 from wtforms.validators import DataRequired
 
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
 
-class SignUpForm(Form):
+class SignUpForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     password2 = PasswordField('password2', validators=[DataRequired()])
 
 
-class ShoppingListForm(Form):
+class ShoppingListForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
 
-class DeleteShoppingListForm(Form):
+class DeleteShoppingListForm(FlaskForm):
     id = StringField('id', validators=[DataRequired()])
 
-class ShoppingListItemForm(Form):
+class ShoppingListItemForm(FlaskForm):
     name = StringField('username', validators=[DataRequired()])
     date = DateField('password', validators=[DataRequired()])
